a09d2a9830379b5280fdec85cb053915db4c2940
IaVe3ynchik <artemmac00@gmail.com>
2024-12-13 19:33:04 +0300

    Создан файл HomeWork4.cs с 4 Домашним заданием

---
+++ HomeWork4.cs
@@ -0,0 +1,33 @@
+using System;
+
+class HomeWork4{
+    public static int[] Merge(int[] a, int[] b){
+        int[] res = new int[a.Length + b.Length];
+        for(int i = 0; i < a.Length; i++){
+            res[i] = a[i];
+        }
+        for(int i = 0; i < b.Length;i++){
+            res[a.Length + i] = b[i];
+        }
+        return res;
+    }
+
+    public static int[] Rotat(int[] a, int i){
+        int[] res = new int[a.Length];
+        for(int n = 0; n < a.Length; n++){
+            if(n + i < a.Length){
+                res[n + i] = a[n];
+            }
+            else{
+                res[n + i - a.Length] = a[n];
+            }
+        }
+        return res;
+    }
+
+    static void Main(){
+        int[] a = Rotat([1,2,3,4,5],2);
+        Console.WriteLine(string.Join(", ", a));
+
+    }
+}
